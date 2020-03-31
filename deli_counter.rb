793acf8e05db450f71f3cd13e11a32170bbcb9ac
aurel fontaine <ajfame11@gmail.com>
2020-03-31 17:29:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,8 +16,9 @@ def take_a_number(katz_deli, name)
    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 def now_serving(katz_deli)
-  if katz_deli == []
+  if katz_deli == [0]
     puts "There is nobody waiting to be served!"
+  else 
   puts "Currently serving #{katz_deli.first}."
 end
 end
